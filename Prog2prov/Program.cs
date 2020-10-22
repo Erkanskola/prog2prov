cdb61cfdec869790f6b65c634464f424c3f8898c
Erik Calissendorff <erik.calissendorff@learnet.se>
2020-10-22 15:10:18 +0200

    initial

---
+++ Prog2prov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog2prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
